03291edf5b7530c12edc578292009e64f608f7d0
Jonathan <jonathan@lawpath.com.au>
2014-05-24 14:57:41 +0800

    require login

--- panel/views.py
+++ panel/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView, FormView
 from .forms import PanelForm
 from mailman.models import WebHandler
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 class PanelIndex(FormView):
     template_name = "panel/index.djhtml"
@@ -17,6 +19,11 @@ class PanelIndex(FormView):
             messages.error(self.request, e)
             self.success_url = '/panel/'
         return super(PanelIndex, self).form_valid(form)
+
+    def render_to_response(self, context, **response_kwargs):
+        if not self.request.user.is_authenticated():
+            return redirect('/')
+        return super(PanelIndex, self).render_to_response(context, **response_kwargs)
 
 class Success(TemplateView):
     template_name = "panel/success.djhtml"
